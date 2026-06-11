879c89ffcb48a7116b25bfc1c137b7cfae3b7c6e
Jack <jackmao.x@gmail.com>
2026-06-10 22:25:59 -0400

    Add files via upload

---
+++ projects/hana/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = "hana-v20";
+const APP_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/favicon-32.png",
+  "./icons/favicon.svg",
+  "./icons/icon.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.png",
+  "./icons/icon-512.svg",
+  "./icons/apple-touch-icon.png",
+  "./icons/apple-touch-icon.svg",
+  "./icons/maskable.svg",
+  "./icons/shortcut-daily.svg",
+  "./icons/shortcut-shadow.svg",
+  "./icons/og-image.png",
+  "./icons/og-image.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
